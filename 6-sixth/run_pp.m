a0bd5538759e9a30a6da05886172433c042153e8
Mohamed Gresha <37029038+MohamedGresha@users.noreply.github.com>
2021-06-30 19:37:43 +0200

    Add files via upload

---
+++ 6-sixth/run_pp.m
@@ -0,0 +1,64 @@
+% run an algorithm to estimate the number of clusters in a data set
+% for 100 times and save the results
+
+%Ahmed Rafat
+%Mohamed Gresha
+%Oct. 18
+
+% frogsmfccs data set
+
+base_path = 'D:\1-Paper\paper 1\00-MATLAB\new\big\2- second_big\1';
+for q=1:10                %run the Algorithm 100 time to 
+    
+    tic
+    
+        [bestk,bestw,ACL,clusterid,NMI,NN]=demo_p_1;     %Parallel Processing
+        % [bestk,bestw,ACL,clusterid,NMI,NN]=demo_s;     %sequention processing
+        
+    
+    time=toc;
+    PP6(q).NN=NN;
+    PP6(q).kopt=bestk;
+    PP6(q).NMI=NMI;
+    PP6(q).bestk=bestk;
+    PP6(q).ACL=ACL;
+    PP6(q).time=time;
+    
+%     NMI_vec=[NMI_vec;NMI];
+%     bestk_vec=[bestk_vec;bestk];
+%     ACLopt=[ACLopt;ACL];
+    %subfolder = ('New folder2');
+    file_name=sprintf('acl_PP%d.mat',q);
+    save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid', 'NMI','time','NN','PP6');
+end
+
+%save(fullfile(base_path,'acl_PP'), 'NN', 'PP6');
+
+%save acl_irs NMI_vec bestk_vec kopt ACLopt;
+
+disp('===========================================');
+% disp('kopt=');
+% disp(length(find(kopt==3)));
+[val,id] = min([PP6.ACL]);
+%[val,id]=min(ACLopt);
+disp('======> Min ACL <======');
+disp('ACL=');
+disp(val);
+disp('kopt=');
+disp(PP6(id).kopt);
+disp('NMIopt=');
+disp(PP6(id).NMI);
+disp('time=');
+disp(PP6(id).time);
+[val,id]=max([PP6.NMI]);
+disp('======> Max NMI<======');
+disp('MAXNMI=');
+disp(val);
+disp('MaxACL=');
+disp(PP6(id).ACL);
+disp('kopt=');
+disp(PP6(id).kopt);
+disp('time=');
+disp(PP6(id).time);
+
+return;
